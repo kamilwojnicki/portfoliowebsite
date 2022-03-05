16a9b3e222e24f3afc7f1c02d4a4f4e9032bf7ac
Kamil <kamil@runspiration.pl>
2022-03-05 14:03:54 +0100

    loading bars js

--- resources/script.js
+++ resources/script.js
@@ -11,4 +11,42 @@ skillsLink.addEventListener('click', function() {
     skills.scrollIntoView({behavior: "smooth"});
 });
 
+let progressHTML = document.querySelector('.html .progress');
+let htmlTarget = document.querySelector('.html img');
+let progressCSS = document.querySelector('.css .progress');
+let cssTarget = document.querySelector('.css img');
+let progressJS = document.querySelector('.js .progress');
+let jsTarget = document.querySelector('.js img');
 
+let loadProgress = (element, number) => {
+    element.style.width = 0;
+    element.animate([{width: 0}, {width: number + '%'}],1000);
+    element.style.width = number + '%';
+}
+
+const loadProgressHtml = function() {
+    loadProgress(progressHTML, 90);
+};
+const loadProgressCSS = function() {
+    loadProgress(progressCSS, 80);
+};
+const loadProgressJS = function() {
+    loadProgress(progressJS, 50);
+};
+
+htmlTarget.addEventListener('click', loadProgressHtml);
+cssTarget.addEventListener('click', loadProgressCSS);
+jsTarget.addEventListener('click', loadProgressJS);
+
+/*
+const loadProgress = function(target, number) {
+    target.style.width = '0%';
+
+    for (let i = 0; i <= number; i++) {
+
+        target.style.width = i + '%';
+        setTimeout(function(){console.log(i);}, 1000);
+    }
+};
+
+loadProgress(progress, 60);*/
